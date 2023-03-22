f1b6f04c13c2f71473440e67a235b94352dc5124
SunshineNURSmile <yuanyiw@bu.edu>
2023-03-22 00:34:04 -0400

    Added Create Recipe API w/o validation

--- backend/routers/RecipeRouter.js
+++ backend/routers/RecipeRouter.js
@@ -6,7 +6,12 @@ import recipeIDValidation from '../middlewares/recipe/recipeIDValidation.js';
 
 const router = express.Router();
 
-router.get('/all', getAllRecipesValidation, RecipeController.getAllRecipes);
+router.get('/', getAllRecipesValidation, RecipeController.getAllRecipes);
+
+// This route needs complex validation logic and needs to upload images to S3 first in createRecipeMiddleware
+// At this stage we only need to make sure there's no script injection into the database
+// But ultimately we need to make sure that there are no profanities in the recipe, and no explicit images
+router.post('/', RecipeController.createRecipe);
 
 router.get('/:recipeId', recipeIDValidation, RecipeController.getRecipeById);
 
