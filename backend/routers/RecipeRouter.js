66873f4402a96dc5ffdb343fd2ca49c07c44362d
SunshineNURSmile <yuanyiw@bu.edu>
2023-03-21 23:57:55 -0400

    Updated Recipe Router

--- backend/routers/RecipeRouter.js
+++ backend/routers/RecipeRouter.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import RecipeController from '../controllers/RecipeController.js';
-const router = express.Router();
 
 import getAllRecipesValidation from '../middlewares/recipe/getAllRecipesValidation.js';
+import recipeIdValidation from '../middlewares/recipe/recipeIdValidation.js';
+
+const router = express.Router();
 
 router.get('/all', getAllRecipesValidation, RecipeController.getAllRecipes);
+
+router.get('/:recipeId', recipeIdValidation, RecipeController.getRecipeById);
+
+// This route needs Admin role
+router.delete(
+  '/:recipeId',
+  recipeIdValidation,
+  RecipeController.deleteRecipeById,
+);
 
 export default router;
