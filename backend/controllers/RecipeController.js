b5b71eee9b8129904b6aa65b74371b28844a8ac7
SunshineNURSmile <yuanyiw@bu.edu>
2023-03-29 17:15:47 -0400

    Added Update Ingredients API

--- backend/controllers/RecipeController.js
+++ backend/controllers/RecipeController.js
@@ -4,6 +4,7 @@ import createRecipe from '../apis/recipe/createRecipe.js';
 import getLatestRecipes from '../apis/recipe/getLatestRecipes.js';
 import getRecipesByCategory from '../apis/recipe/getRecipesByCategory.js';
 import getRecipesByAuthor from '../apis/recipe/getRecipesByAuthor.js';
+import updateIngredients from '../apis/recipe/updateIngredients.js';
 
 const RecipeController = {
   getRecipeById,
@@ -12,6 +13,7 @@ const RecipeController = {
   getLatestRecipes,
   getRecipesByCategory,
   getRecipesByAuthor,
+  updateIngredients,
 };
 
 export default RecipeController;
