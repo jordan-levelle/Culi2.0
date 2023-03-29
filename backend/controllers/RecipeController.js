148e484a51b22ff60a762ecb478829a6ab2b31ea
SunshineNURSmile <yuanyiw@bu.edu>
2023-03-29 16:16:14 -0400

    Created Two Filter Recipes API

--- backend/controllers/RecipeController.js
+++ backend/controllers/RecipeController.js
@@ -1,13 +1,17 @@
-import getAllRecipes from '../apis/recipe/getAllRecipes.js';
 import getRecipeById from '../apis/recipe/getRecipeById.js';
 import deleteRecipe from '../apis/recipe/deleteRecipe.js';
 import createRecipe from '../apis/recipe/createRecipe.js';
+import getLatestRecipes from '../apis/recipe/getLatestRecipes.js';
+import getRecipesByCategory from '../apis/recipe/getRecipesByCategory.js';
+import getRecipesByAuthor from '../apis/recipe/getRecipesByAuthor.js';
 
 const RecipeController = {
-  getAllRecipes,
   getRecipeById,
   deleteRecipe,
   createRecipe,
+  getLatestRecipes,
+  getRecipesByCategory,
+  getRecipesByAuthor,
 };
 
 export default RecipeController;
--- backend/routers/RecipeRouter.js
+++ backend/routers/RecipeRouter.js
@@ -1,19 +1,31 @@
 import express from 'express';
 import RecipeController from '../controllers/RecipeController.js';
 
-import getAllRecipesValidation from '../middlewares/recipe/getAllRecipesValidation.js';
+import filterRecipesValidation from '../middlewares/recipe/filterRecipesValidation.js';
 import recipeIDValidation from '../middlewares/recipe/recipeIDValidation.js';
 
 const router = express.Router();
 
-router.get('/', getAllRecipesValidation, RecipeController.getAllRecipes);
+router.get(
+  '/category',
+  filterRecipesValidation,
+  RecipeController.getRecipesByCategory,
+);
+
+router.get(
+  '/author',
+  filterRecipesValidation,
+  RecipeController.getRecipesByAuthor,
+);
+
+router.get('/latest', RecipeController.getLatestRecipes);
+
+router.get('/:recipeId', recipeIDValidation, RecipeController.getRecipeById);
 
 // This route needs complex validation logic and needs to upload images to S3 first in createRecipeMiddleware
 // At this stage we only need to make sure there's no script injection into the database
 // But ultimately we need to make sure that there are no profanities in the recipe, and no explicit images
 router.post('/', RecipeController.createRecipe);
-
-router.get('/:recipeId', recipeIDValidation, RecipeController.getRecipeById);
 
 // This route needs Admin role
 router.delete('/:recipeId', recipeIDValidation, RecipeController.deleteRecipe);
