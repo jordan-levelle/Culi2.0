a2578cc908aecf90e3b1a1c227ae078b44738972
SunshineNURSmile <yuanyiw@bu.edu>
2023-03-29 16:51:14 -0400

    Added Response For Errors

--- backend/apis/recipe/getRecipesByCategory.js
+++ backend/apis/recipe/getRecipesByCategory.js
@@ -15,7 +15,7 @@ const getRecipesByCategory = async (req, res) => {
 
     res.status(200).json(recipes);
   } catch (err) {
-    console.error(err);
+    res.status(500).json({ message: err.message });
   }
 };
 
