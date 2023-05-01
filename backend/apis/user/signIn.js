88d4a0929c49bed22371b3d1cd8e5361d2accc30
SunshineNURSmile <yuanyiw@bu.edu>
2023-05-01 19:43:27 -0400

    Changed request body structure

--- backend/apis/user/signIn.js
+++ backend/apis/user/signIn.js
@@ -1,21 +1,21 @@
 import User from '../../models/User.js';
 
 const signIn = async (req, res) => {
-  const { user } = req.body;
+  const { uid, username, email, profilePicture, accessToken } = req.body;
 
   try {
-    const existingUser = await User.findOne({ uid: user.uid });
+    const existingUser = await User.findOne({ uid });
 
     if (!existingUser) {
       await User.create({
-        uid: user.uid,
-        username: user.displayName,
-        email: user.email,
-        profileImage: user.photoURL,
+        uid,
+        username,
+        email,
+        profilePicture,
       });
     }
 
-    res.cookie('token', user.accessToken, { httpOnly: true });
+    res.cookie('token', accessToken, { httpOnly: true });
     res.status(200).json({ message: 'User signed in successfully!' });
   } catch (err) {
     res.status(500).json({ message: err.message });
