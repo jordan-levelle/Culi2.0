b8c3554eaed71bd6b2d709dac544fc9d0d7b18e6
SunshineNURSmile <yuanyiw@bu.edu>
2023-05-01 19:44:40 -0400

    Added sign in validation middleware

--- backend/routers/UserRouter.js
+++ backend/routers/UserRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import UserController from '../controllers/UserController.js';
 
+import signInValidation from '../middlewares/user/signInValidation.js';
+
 const router = express.Router();
 
-router.post('/signin', UserController.signIn);
+router.post('/signin', signInValidation, UserController.signIn);
 
 router.post('/signout', UserController.signOut);
 
