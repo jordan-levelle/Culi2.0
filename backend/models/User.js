5588f1064a518f2238ec772b4b204f1a7b95c607
SunshineNURSmile <yuanyiw@bu.edu>
2023-05-01 19:16:15 -0400

    Added Firebase uid

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  uid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   username: {
     type: String,
     required: true,
