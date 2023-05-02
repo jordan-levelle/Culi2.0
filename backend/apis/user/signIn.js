8f53467bda7ea0fbc41f011f0f0761ce4a025230
SunshineNURSmile <yuanyiw@bu.edu>
2023-05-01 20:33:54 -0400

    Changed variable name

--- backend/apis/user/signIn.js
+++ backend/apis/user/signIn.js
@@ -1,7 +1,7 @@
 import User from '../../models/User.js';
 
 const signIn = async (req, res) => {
-  const { uid, username, email, profilePicture, accessToken } = req.body;
+  const { uid, username, email, profilePicture, token } = req.body;
 
   try {
     const existingUser = await User.findOne({ uid });
@@ -15,7 +15,7 @@ const signIn = async (req, res) => {
       });
     }
 
-    res.cookie('token', accessToken, { httpOnly: true });
+    res.cookie('token', token, { httpOnly: true });
     res.status(200).json({ message: 'User signed in successfully!' });
   } catch (err) {
     res.status(500).json({ message: err.message });
